dc98a6e576e6181e7c32ad81eb59962106a5d734
SuHyun <jish2689@gmail.com>
2021-09-25 00:28:13 +0900

    Querydsl 실습 DTO 객체 생성, Service 생성 후 test

---
+++ src/test/java/org/zerock/guestbook/repository/GuestbookRepositoryTests.java
@@ -0,0 +1,89 @@
+package org.zerock.guestbook.repository;
+
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.zerock.guestbook.entity.Guestbook;
+import org.zerock.guestbook.entity.QGuestbook;
+
+
+
+import java.util.Optional;
+import java.util.stream.IntStream;
+
+@SpringBootTest
+public class GuestbookRepositoryTests {
+    @Autowired
+    private GuestbookRepository guestbookRepository;
+
+    @Test
+    public void insertDummies(){
+        IntStream.rangeClosed(1, 300).forEach(i -> {
+            Guestbook guestbook = Guestbook.builder().title("Title..." + i).content("Content..."+i).writer("user"+(i%10)).build();
+            System.out.println(guestbookRepository.save(guestbook));
+        });
+    }
+
+    @Test
+    public void updateTest(){
+        Optional<Guestbook> result = guestbookRepository.findById(300L);
+
+        if(result.isPresent()){
+            Guestbook guestbook = result.get();
+            guestbook.changeTitle("Changed Title...");
+            guestbook.changeContent("Changed Content...");
+            guestbookRepository.save(guestbook);
+        }
+    }
+
+    // 단일 항목 검색 조건 테스트
+    @Test
+    public void testQuery1(){
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("gno").descending());
+
+        //Predicate
+        QGuestbook qGuestbook = QGuestbook.guestbook;
+
+        BooleanBuilder booleanBuilder = new BooleanBuilder();
+        // title like %1%
+        BooleanExpression expression = qGuestbook.title.contains("1");
+
+        booleanBuilder.and(expression);
+
+        Page<Guestbook> result = guestbookRepository.findAll(booleanBuilder, pageable);
+
+        result.forEach(guestbook -> {
+            System.out.println(guestbook);
+        });
+    }
+
+    @Test
+    public void testQuery2(){
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("gno").descending());
+
+        QGuestbook qGuestbook = QGuestbook.guestbook;
+
+        BooleanBuilder booleanBuilder = new BooleanBuilder();
+
+        // title like %1% or content like %1%
+        BooleanExpression expression = qGuestbook.title.contains("1");
+
+        BooleanExpression exAll = expression.or(qGuestbook.content.contains("1"));
+
+        booleanBuilder.and(exAll);
+
+        booleanBuilder.and(qGuestbook.gno.gt(0L));
+
+        Page<Guestbook> result = guestbookRepository.findAll(booleanBuilder, pageable);
+
+        result.forEach(guestbook -> {
+            System.out.println(guestbook);
+        });
+    }
+}
